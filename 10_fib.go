4360c6fedf5b3387c45cc0bf58ed3ee6287d6e61
sun.xing <xing.sun@easystack.cn>
2021-03-18 20:08:42 +0800

    10_fib

---
+++ 10_fib.go
@@ -0,0 +1,49 @@
+package Leet_Code
+// 写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项（即 F(N)）。斐波那契数列的定义如下：
+//
+// F(0) = 0,   F(1) = 1
+// F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
+// 斐波那契数列由 0 和 1 开始，之后的斐波那契数就是由之前的两数相加而得出。
+//
+// 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+//
+// 示例 1：
+// 输入：n = 2
+// 输出：1
+
+// 示例 2：
+// 输入：n = 5
+// 输出：5
+
+
+
+// 方法1，直接递归，效率太低
+func Fib(n int) int {
+	if n ==0 {
+		return 0
+	}
+	if n ==1{
+		return 1
+	}
+	return Fib(n-1) + Fib(n-2)
+}
+
+// 方法2，重复计算太多，尽量避免重复计算，从下往上算，时间复杂度为O（n）
+func Fib2(n int) int {
+	if n ==0 {
+		return 0
+	} else if n ==1{
+		return 1
+	} else {
+		fib0 := 0
+		fib1 := 1
+		var fibX int
+		for i:=2;i<=n;i++{
+			fibX = fib0+fib1
+			fib0 = fib1
+			fib1 = fibX
+		}
+		return fibX
+	}
+
+}
